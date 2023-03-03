72e5db5b497e24341c6b387cfe95cfa686badc51
wangzhenxuan <wangzhenxuan@sinosoft.com.cn>
2023-03-03 18:24:34 +0800

    医院信息设置模块

--- service/service_hosp/src/main/java/com/wzx/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/wzx/yygh/hosp/ServiceHospApplication.java
@@ -1,13 +1,12 @@
-package com.wzx.yygh_parent;
+package com.wzx.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class YyghParentApplication {
-
+public class ServiceHospApplication {
     public static void main(String[] args) {
-        SpringApplication.run(YyghParentApplication.class, args);
+        SpringApplication.run(ServiceHospApplication.class, args);
     }
-
 }
